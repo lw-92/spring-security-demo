5ddab39d6d76680eb2fea8379538976f11579577
liwei <670415962@qq.com>
2020-06-04 22:01:32 +0800

    项目初始化

---
+++ spring-security-demo/src/main/java/com/lee/SpringSecurityDemoApp.java
@@ -0,0 +1,12 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApp.class, args);
+    }
+}
