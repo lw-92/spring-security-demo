78ee65e13ff79ca747a2dff040a4c94762e070db
liwei <670415962@qq.com>
2020-06-06 22:56:25 +0800

    自定义登录失败和权限认证失败的处理

---
+++ spring-security-demo/src/main/java/com/lee/configuration/MyAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.lee.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+@Component
+public class MyAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    //未登录
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        HashMap<String, String> map = new HashMap<>(2);
+        map.put("uri", request.getRequestURI());
+        map.put("msg", "认证失败");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding("utf-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String resBody = objectMapper.writeValueAsString(map);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(resBody);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
--- spring-security-demo/src/main/java/com/lee/configuration/SecurityConfig.java
+++ spring-security-demo/src/main/java/com/lee/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.lee.configuration;
 
+import com.lee.auth.MyAccessDeniedHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,11 +18,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private MyAccessDeniedHandler myAccessDeniedHandler;
+
+    @Autowired
+    private MyAuthenticationEntryPoint myAuthenticationEntryPoint;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-//                .anonymous().and()
                 .authorizeRequests()// 授权配置
                 .antMatchers("/noauth/**").anonymous() //注意要以'/' 开头  匿名用户可以访问，
                 .antMatchers("/admin/**").hasRole("admin") //admin必须要有admin角色才能访问
@@ -32,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin() // 登录配置，这里会加入一个UsernamePasswordAuthenticationFilter
                 .and()
                 .httpBasic();  //未登录时，给一个匿名用户
+//        http.exceptionHandling().accessDeniedHandler(myAccessDeniedHandler).authenticationEntryPoint(myAuthenticationEntryPoint);
     }
 
     @Bean
